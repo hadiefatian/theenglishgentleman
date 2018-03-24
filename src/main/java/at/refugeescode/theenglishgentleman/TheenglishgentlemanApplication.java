6781a979293239479a9e521b043a1ec65553a448
Hadi <hadi.effatian@gmail.com>
2018-03-24 19:55:56 +0100

    Create Project

---
+++ src/main/java/at/refugeescode/theenglishgentleman/TheenglishgentlemanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.theenglishgentleman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheenglishgentlemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheenglishgentlemanApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/theenglishgentleman/TheenglishgentlemanApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.theenglishgentleman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheenglishgentlemanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
